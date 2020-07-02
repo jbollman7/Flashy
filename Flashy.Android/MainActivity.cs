18f86b7b379b652721e2fe55e6ab9dcfe3364641
jumpman <joseph.bollman@gmail.com>
2020-07-01 22:25:51 -0600

    Deleted several folders and the default png's. Most likely will cause an error later. Have no idea why i did it. New app icon in place. Expanded target android verison, and set a low minimum version for maxiumum audience.

--- Flashy.Android/MainActivity.cs
+++ Flashy.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Flashy.Droid
 {
-    [Activity(Label = "Flashy", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Flashy",  Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
